ba2cd763a09c2edf65713925ece5f5cca37f953d
Llanasl2 <54413591+Llanasl2@users.noreply.github.com>
2022-06-30 10:08:10 -0500

    Baseline before rearranging loops

--- Coding Tests/Threading/Threading/main.cpp
+++ Coding Tests/Threading/Threading/main.cpp
@@ -2,24 +2,39 @@
 #include <mutex>
 #include <thread>
 #include <vector>
+#include <condition_variable>
+#include <chrono>
+#include <atomic>
+
 using namespace std;
 
 //Allows for safe access to shared resource
-mutex IncLock;
+mutex m;
+condition_variable tLock;
+atomic <int> shared(22);
+bool ready =false;
+bool processed = false;
+bool complete = false;
 
-void IncrementCount(int id, int &shared)
+void IncrementCount(int id, int Loops, int Threads)
 {
-	lock_guard<mutex> lock(IncLock);
+	unique_lock<mutex> lk(m);
+	tLock.wait(lk, [] {return ready; });
 	cout << "Thread " << id << " Value: " << ++shared << endl;
-
+	processed = true;
+	lk.unlock();
+	processed = false;
+	if (shared == shared + Loops)
+	{
+		complete = true;
+	}
+	tLock.notify_all();
 }
 int main(int argc, char** argv)
 {
 	int ThreadCount = atoi(argv[1]);
 	int LoopCount = atoi(argv[2]);
 	cout <<  "ArgCount: " << argc <<"\nThreadCount: " << ThreadCount << endl << "LoopCount: " << LoopCount << endl;
-
-	int shared = 22;
 	vector<thread> ThreadVec;
 	int j = 0;
 	for (int i = 0; i < LoopCount; i++)
@@ -28,9 +43,16 @@ int main(int argc, char** argv)
 		{
 			j = 0;
 		}
-		ThreadVec.push_back(thread(IncrementCount, j, ref(shared)));
+		ThreadVec.push_back(thread(IncrementCount, j,LoopCount,ThreadCount));
 		j++;
 	}
+	ready = true;
+	cout << "Waiting 10 seconds" <<endl;
+	//this_thread::sleep_for(chrono::milliseconds(10000));
+	tLock.notify_all();
+
+	unique_lock<mutex> lk(m);
+	tLock.wait(lk, [] {return complete;});
 
 	for (int i = 0; i < ThreadVec.size(); i++)
 	{
