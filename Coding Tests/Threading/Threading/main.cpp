ea2c64e2edb8ecd4670783ccbf3404f0969b9576
Llanasl2 <54413591+Llanasl2@users.noreply.github.com>
2022-06-30 12:20:04 -0500

    Complete Solution..hopefully

--- Coding Tests/Threading/Threading/main.cpp
+++ Coding Tests/Threading/Threading/main.cpp
@@ -8,58 +8,64 @@
 
 using namespace std;
 
-//Allows for safe access to shared resource
+//Allows for safe access to shared resources
 mutex m;
 condition_variable tLock;
-atomic <int> shared(22);
+atomic <int> shared(0);
+
+//Bools to signal thread begin and completion of execution
 bool ready =false;
-bool processed = false;
 bool complete = false;
 
+//Increment the shared value and output your unique identifier
 void IncrementCount(int id, int Loops, int Threads)
 {
+	//Lock the thread until signaled by main thread with the "ready" bool.
 	unique_lock<mutex> lk(m);
 	tLock.wait(lk, [] {return ready; });
+
 	cout << "Thread " << id << " Value: " << ++shared << endl;
-	processed = true;
+	
 	lk.unlock();
-	processed = false;
-	if (shared == shared + Loops)
+	
+	//Signal that the threads have run for the amount of time desired.
+	if (shared == ((Loops * Threads)))
 	{
 		complete = true;
 	}
+
+	//Notify any waiting thread that they can begin execution
 	tLock.notify_all();
 }
 int main(int argc, char** argv)
 {
+	//Grab information from the command line and parse to interger
 	int ThreadCount = atoi(argv[1]);
 	int LoopCount = atoi(argv[2]);
-	cout <<  "ArgCount: " << argc <<"\nThreadCount: " << ThreadCount << endl << "LoopCount: " << LoopCount << endl;
+
+	//loop through Z amount of times for X amount of threads
 	vector<thread> ThreadVec;
-	int j = 0;
 	for (int i = 0; i < LoopCount; i++)
 	{
-		if (j == ThreadCount)
+		for (int j = 0; j < ThreadCount; j++)
 		{
-			j = 0;
+			ThreadVec.push_back(thread(IncrementCount, j, LoopCount, ThreadCount));
 		}
-		ThreadVec.push_back(thread(IncrementCount, j,LoopCount,ThreadCount));
-		j++;
 	}
+	//Set parameter to release threads for execution
 	ready = true;
 	cout << "Waiting 10 seconds" <<endl;
-	//this_thread::sleep_for(chrono::milliseconds(10000));
-	tLock.notify_all();
+	this_thread::sleep_for(chrono::milliseconds(10000));
 
+	//notify all threads that they are waiting for execution, and wait until complete signal is given
+	tLock.notify_all();
 	unique_lock<mutex> lk(m);
 	tLock.wait(lk, [] {return complete;});
 
+	//join the threads
 	for (int i = 0; i < ThreadVec.size(); i++)
 	{
 		ThreadVec[i].join();
 	}
-
-	system("Pause");
-
-
+	return 0;
 }
